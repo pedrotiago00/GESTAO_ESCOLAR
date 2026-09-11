7f3968201c94df3d957152399a0f50c7d5b4a4a4
Pedro Tiago <pedro005112@gmail.com>
2026-09-11 08:28:32 -0300

    Apagando o arquivo .env do git hub e corrigindo a arquitetura do projeto

--- workspace/settings.py
+++ workspace/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv
+import os
 
 # Load environment variables from .env file
 load_dotenv()
