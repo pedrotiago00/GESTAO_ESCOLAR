c3a8f4ed148daed1e15830fe9a414db48ba4f9e2
Pedro Tiago da Cruz Moreira <pedro005112@gmail.com>
2026-09-14 23:36:15 -0300

    Algumas mudanças nada demais

--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.hashers import make_password
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import LoginForms, CadastroForms
 
 def login(request):
@@ -8,4 +8,9 @@ def login(request):
 
 def cadastro(request):
     form = CadastroForms()
+    if request.method == 'POST':
+        form = CadastroForms(request.POST)
+        if form['password'].data != form['password_confirm'].data:
+            return redirect('cadastro')
+
     return render(request, 'usuarios/cadastro.html', {'form': form})
